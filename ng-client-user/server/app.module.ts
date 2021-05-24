ee496f311c62b1f12e3bad084eb6503761b0fcff
Axmouth <aggronfg@yahoo.com>
2021-05-24 03:12:33 +0300

    Change to nestjs for ssr to use the cache feature

---
+++ ng-client-user/server/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
+import { AppServerModule } from '../src/main.server';
+
+@Module({
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/ng-user-client/browser'),
+      cache: true,
+    }),
+  ],
+})
+export class AppModule {}
